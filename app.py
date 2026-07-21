3a38114a6816b2420988ef28eae7edd94fa4834d
AmartyaDas01 <dasamartya2000@gmail.com>
2026-07-21 14:46:15 +0530

    Prepare app for Render deployment: bind to PORT env var, add render.yaml blueprint

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 """Gradio demo: draw a digit, get a prediction from an MNIST-trained CNN."""
 
+import os
+
 import numpy as np
 import gradio as gr
 from PIL import Image
@@ -97,4 +99,4 @@ with gr.Blocks(title="Handwritten Digit Recognition") as demo:
     sketchpad.change(predict, inputs=sketchpad, outputs=label)
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
